8bfff764a4b31cd6ed0553a07b7c0b9bd85f94c0
Rost <rzhistovskiy@inno.tech>
2024-10-27 00:11:50 +0300

    BPP

--- src/main/java/dev/rost/aspects/ez/EzServiceBPP.java
+++ src/main/java/dev/rost/aspects/ez/EzServiceBPP.java
@@ -4,22 +4,35 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 class EzServiceBPP implements BeanPostProcessor {
 
+    private static final String ezServiceBeanName = "ezServiceImpl";
+    private static EzInfo ezInfo;
+
+
     @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (beanName.equals("ezService")) {
-            Enhancer enhancer = new Enhancer();
-            enhancer.setSuperclass(bean.getClass());
-            enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+    public Object postProcessBeforeInitialization(@NonNull Object bean, @NonNull String beanName) throws BeansException {
+        if (ezServiceBeanName.equals(beanName))
+            ezInfo = new EzInfo(bean.getClass(), bean.getClass().getInterfaces());
+        return bean;
+    }
+
+
+    @Override
+    public Object postProcessAfterInitialization(@NonNull Object bean, String beanName) throws BeansException {
+        if (beanName.equals("ezServiceImpl"))
+            return Enhancer.create(ezInfo.clazz, ezInfo.interfaces, (MethodInterceptor) (obj, method, args, proxy) -> {
                 System.out.println("EzService#proxyMethodInvocation");
                 return proxy.invoke(bean, args);
             });
-            return enhancer.create();
-        }
+
         return bean;
     }
+
+
+    record EzInfo(Class<?> clazz, Class<?>[] interfaces) {}
 }
