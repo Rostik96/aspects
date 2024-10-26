e9d6e4380e72747bdb8a0a95d48750bdaa97a6a6
Ivanov Vitalii (4113013) <vaivanov@dev.vtb.ru>
2024-10-26 21:16:59 +0300

    creating_proxy = true

--- src/main/java/dev/rost/aspects/ez/EzServiceBPP.java
+++ src/main/java/dev/rost/aspects/ez/EzServiceBPP.java
@@ -2,22 +2,24 @@ package dev.rost.aspects.ez;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.stereotype.Component;
-
-
 
 @Component
 class EzServiceBPP implements BeanPostProcessor {
 
-
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return beanName.equals("ezService")
-                ? Proxy.newProxyInstance(bean.getClass().getClassLoader(), bean.getClass().getInterfaces(),
-                (proxy, method, args) -> {
-                    System.out.println("EzService#proxyMethodInvocation");
-                    return method.invoke(bean, args);
-                }
-                : bean;
+        if (beanName.equals("ezService")) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(bean.getClass());
+            enhancer.setCallback((MethodInterceptor) (obj, method, args, proxy) -> {
+                System.out.println("EzService#proxyMethodInvocation");
+                return proxy.invoke(bean, args);
+            });
+            return enhancer.create();
+        }
+        return bean;
     }
 }
